3eefa1b69efb0f5f91d26de86ee84103679f4b7d
CttB2012 <109867823+CttB2012@users.noreply.github.com>
2024-12-21 18:23:47 -0300

    primeiro commit

---
+++ src/main/java/com/projeto/agendadornotificacao/AgendadornotificacaoApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.agendadornotificacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadornotificacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendadornotificacaoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/agendadornotificacao/AgendadornotificacaoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.agendadornotificacao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadornotificacaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
